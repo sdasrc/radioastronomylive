c8e90b2d8a3cb890660fa8310e331e4e5ab698b2
admiralprice <soumyadeep.das.phy14@iitbhu.ac.in>
2021-01-07 11:47:06 +0530

    Added timestamp and tweet counts

--- retweet.py
+++ retweet.py
@@ -6,6 +6,7 @@
 
 import tweepy
 from time import sleep
+from datetime import datetime
 # Import in your Twitter application keys, tokens, and secrets.
 # Make sure your keys.py file lives in the same directory as this .py file.
 # from keys import *
@@ -19,7 +20,12 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-print('Starting bot')
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print('Starting bot : '+dt_string)
+
+validretweet = 0
+failedtweet = 0
 
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
@@ -30,6 +36,7 @@ for tweet in tweepy.Cursor(api.search, q='%23radioastronomy%20OR%20%23radioastro
 
         tweet.retweet()
         print('Retweet published successfully.')
+        validretweet = validretweet + 1
 
         # Where sleep(10), sleep is measured in seconds.
         # Change 10 to amount of seconds you want to have in-between retweets.
@@ -40,8 +47,9 @@ for tweet in tweepy.Cursor(api.search, q='%23radioastronomy%20OR%20%23radioastro
     except tweepy.TweepError as error:
         print('\nError. Retweet not successful. Reason: ')
         print(error.reason)
+        failedtweet = failedtweet + 1
 
     except StopIteration:
         break
 
-print('End bot run')
+print('End bot run. Retweet : '+str(validretweet)+', failed : '+str(failedtweet))
